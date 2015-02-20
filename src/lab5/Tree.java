8a0e83292b4e124972034e5c0516fd695c4a45b4
Mauritz <mauritzz@kth.se>
2015-02-20 01:32:24 +0100

    Tree done, tests passing

--- src/lab5/Tree.java
+++ src/lab5/Tree.java
@@ -1,17 +1,35 @@
 package lab5;
 
+/**
+ * A Tree data structure implementation.
+ *
+ * @param <T>		Java Generics data type
+ * @author			Mauritz Zachrisson
+ */
 public class Tree<T extends Comparable<T>>
 {
 	private Node<T> root = null;
 	private int size = 0;
 
+	// Private Node helper class
 	private class Node<N extends Comparable<N>>
 	{
 		N data;
-		Node<N> left;
-		Node<N> right;
+		Node<N> left = null;
+		Node<N> right = null;
+
+		public Node(N data)
+		{
+			this.data = data;
+		}
 	}
 
+	/**
+	 * Check whether the tree contains a given data piece.
+	 *
+	 * @param data		Data to search for
+	 * @return			true if found, false otherwise
+	 */
 	public boolean contains(T data)
 	{
 		// Null check
@@ -22,7 +40,7 @@ public class Tree<T extends Comparable<T>>
 
 		// Iterate down the tree
 		while (node != null)
-
+		{
 			// If the node is found, return true
 			if (data.compareTo(node.data) == 0)
 				return true;
@@ -34,54 +52,168 @@ public class Tree<T extends Comparable<T>>
 				// Otherwise, go right
 			else
 				node = node.right;
+		}
 
 		// If search found nothing, return false
 		return false;
 	}
 
+	/**
+	 * Add a node to the tree.
+	 *
+	 * @param data		Data to insert
+	 * @return			True on insert, false on duplicate
+	 */
 	public boolean add(T data)
 	{
-		// TODO
+		// Null addition is prohibited
+		if (data == null)
+			return false;
 
-		return false;
+		// If root element is null, create it
+		if (root == null)
+		{
+			root = new Node<>(data);
+			size++;
+			return true;
+		}
+
+		// Start search at root node
+		Node<T> node = root;
+
+		// Iterate down the tree
+		while (true)
+		{
+			// If node is found, return false (prohibit duplicate)
+			if (data.compareTo(node.data) == 0)
+				return false;
+
+			// If node is greater than data, go left
+			else if (data.compareTo(node.data) < 0)
+			{
+				// If left child is null, a leaf is reached and data should be added
+				if (node.left == null)
+				{
+					node.left = new Node<>(data);
+					size++;
+					return true;
+				}
+
+				// Otherwise, continue searching
+				else node = node.left;
+			}
+
+			// Otherwise, go right
+			else
+			{
+				// If right child is null, a leaf is reached and data should be added
+				if (node.right == null)
+				{
+					node.right = new Node<>(data);
+					size++;
+					return true;
+				}
+
+				// Otherwise, continue searching
+				else node = node.right;
+			}
+		}
 	}
 
+	/**
+	 * Get the number of elements currently in the tree.
+	 *
+	 * @return 		Tree size as an integer
+	 */
 	public int size()
 	{
-		// TODO
-		return 0;
+		return size;
 	}
 
+	/**
+	 * Calculate the depth of the tree, i.e. the number of branches.
+	 *
+	 * @return		Tree depth as an integer
+	 */
 	public int depth()
 	{
-		// TODO
-		return 0;
+		return depthCounter(root);
 	}
 
+	// Private depth helper method
+	private int depthCounter(Node<T> node)
+	{
+		// Nothing here
+		if (node == null)
+			return 0;
+
+		// Calculate depth of sub-branches
+		int left = depthCounter(node.left);
+		int right = depthCounter(node.right);
+
+		// Return result
+		return Math.max(left, right) + 1;
+	}
+
+	/**
+	 * Calculate the tree leaf count, i.e. the number of nodes with no children.
+	 *
+	 * @return		Tree leaf count as an integer
+	 */
 	public int leaves()
 	{
+		// Generate number of leaves and return
 		return leavesCounter(root);
 	}
 
-	private boolean isEmpty()
-	{
-		return (root == null);
-	}
-
-	@Override
-	public String toString()
-	{
-		// TODO
-		return null;
-	}
-
+	// Private leaf count helper method
 	private int leavesCounter(Node<T> node)
 	{
+		// Nothing here
 		if (node == null)
 			return 0;
+
+		// If leaf, increase leaf counter
 		if (node.left == null && node.right == null)
 			return 1;
 
+		// Continue searching the tree
 		return leavesCounter(node.left) + leavesCounter(node.right);
+	}
+
+	/**
+	 * Generate a sorted String representation of the tree.
+	 */
+	@Override
+	public String toString()
+	{
+		// Generate empty response for empty trees
+		if (size == 0)
+			return "[]";
+
+		// Set up StringBuilder
+		StringBuilder sb = new StringBuilder();
+
+		// Generate the response String
+		buildString(root, sb);
+		sb.setLength(sb.length() - 2);
+
+		// Return the formatted response
+		return String.format("[%s]", sb);
+	}
+
+	// Private toString helper method
+	private void buildString(Node<T> node, StringBuilder sb)
+	{
+		// If left child exists, traverse it
+		if (node.left != null)
+			buildString(node.left, sb);
+
+		// Perform visit action, i.e. add the data
+		sb.append(node.data).append(", ");
+
+		// If right child exists, traverse it
+		if (node.right != null)
+			buildString(node.right, sb);
 	}
 }
--- test/lab5/TreeTest.java
+++ test/lab5/TreeTest.java
@@ -42,7 +42,21 @@ public class TreeTest
 		assertTrue(t.add(4));
 		assertTrue(t.add(5));
 		assertTrue(t.add(6));
-		assertTrue(t.add(null));
+
+		// Assert addition of big integers is possible
+		assertTrue(t.add(2247));
+		assertTrue(t.add(100));
+
+		// Assert that adding them again does not work
+		assertFalse(t.add(100));
+		assertFalse(t.add(2247));
+
+		// Assert addition of negative integers works
+		assertTrue(t.add(-2247));
+		assertFalse(t.add(-2247));
+
+		// Addition of null is prohibited
+		assertFalse(t.add(null));
 	}
 
 	@Test
@@ -79,20 +93,25 @@ public class TreeTest
 		t2.add(6); // 8.left
 		t2.add(4); // 6.left => depth = 4
 
-		assertEquals(4, t2.leaves());
+		assertEquals(1, t2.leaves());
 	}
 
 	@Test
 	public void testToString()
 	{
+		// Check sequentially built tree
 		assertEquals("[1, 2, 3]", t.toString());
 
+		// Check non-sequentially built tree
 		Tree<Integer> t2 = new Tree<>();
-		t2.add(20); // Root node
-		t2.add(10); // Root node
-		t2.add(2); // Root node
-		t2.add(15); // Root node
-
+		t2.add(20);
+		t2.add(10);
+		t2.add(2);
+		t2.add(15);
 		assertEquals("[2, 10, 15, 20]", t2.toString());
+
+		// Test empty tree
+		Tree<Integer> t3 = new Tree<>();
+		assertEquals("[]", t3.toString());
 	}
 }
